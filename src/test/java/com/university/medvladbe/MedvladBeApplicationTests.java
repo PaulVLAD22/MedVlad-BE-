2f2698a3adf59acc6fe3c595347e864a5ddde878
Vlad Munteanu <paulvlad34@gmail.com>
2021-08-20 14:08:31 +0300

    Add files via upload

---
+++ src/test/java/com/university/medvladbe/MedvladBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.medvladbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedvladBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
