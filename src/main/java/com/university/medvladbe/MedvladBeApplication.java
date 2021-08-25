be7ba48abbb8434028762cd6857f57c6199c46a7
paulvlad22 <paulvlad34@gmail.com>
2021-08-25 10:40:27 +0300

    solved prerequisite bug

--- src/main/java/com/university/medvladbe/MedvladBeApplication.java
+++ src/main/java/com/university/medvladbe/MedvladBeApplication.java
@@ -1,7 +1,7 @@
 package com.university.medvladbe;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class MedvladBeApplication {
