6bb29a7b4d9c6c2b3f9f3be863fa90a67eae98e0
Vlad Munteanu <paulvlad34@gmail.com>
2021-09-13 09:46:15 +0300

    added cors filter class

--- src/main/java/com/university/medvladbe/MedvladBeApplication.java
+++ src/main/java/com/university/medvladbe/MedvladBeApplication.java
@@ -2,12 +2,25 @@ package com.university.medvladbe;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MedvladBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MedvladBeApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
--- src/main/java/com/university/medvladbe/controller/TestController.java
+++ src/main/java/com/university/medvladbe/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.university.medvladbe.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping(value="/sample", method = RequestMethod.GET, produces = "application/json")
     @GetMapping("/cox")
     public String getCox(){
         return "COX";
